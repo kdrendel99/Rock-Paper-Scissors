5b2aeed6b2794cf2bfc84c1d62ccbdba9cb85dff
Karlson Drendel <kdrendel99@gmail.com>
2021-04-18 14:23:14 -0700

    Write first test method for P1 property of Game class

--- RockPaperScissors.Tests/ModelTests/GameTests.cs
+++ RockPaperScissors.Tests/ModelTests/GameTests.cs
@@ -13,5 +13,20 @@ namespace RockPaperScissors.Tests
       Game newGame = new Game();
       Assert.AreEqual(typeof(Game),newGame.GetType());
     }
+
+    [TestMethod]
+    public void GetPlayerOne_ReturnsPlayerOne_String()
+    {
+      // Arrange
+      string selection1 = "rock";
+      string selection2 = "paper";
+
+      //Act
+      Game newGame = new Game(selection1,selection2);
+      string playerOne = Game.PlayerOne;
+
+      //Assert
+      Assert.AreEqual(selection1, playerOne);
+    }
   }
 }
